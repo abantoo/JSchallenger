e0ee2fa375fa613ece968cb776424fd031228b0e
abantoo <abantonaimur@gmail.com>
2021-11-17 18:28:41 +0600

    Latest

---
+++ Dates/DaysBetweenTwoDates.js
@@ -0,0 +1,6 @@
+// Write a function that takes two date instances as argument
+// It should return the number of days that lies between those dates
+
+function myFunction(date1, date2) {
+  return Math.abs((date1.getTime() - date2.getTime()) / (24 * 3600 * 1000));
+}
---
+++ Dates/TwoEqualDates.js
@@ -0,0 +1,8 @@
+// Sounds easy, but you need to know the trick
+// Write a function that takes two date instances as argument
+// It should return true if the dates are equal
+// It should return false otherwise
+
+function myFunction(date1, date2) {
+  return date1.getTime() === date2.getTime();
+}

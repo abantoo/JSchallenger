825ebceb26b1b3ff9028a432b5981791ff3f8dbe
abantoo <abantonaimur@gmail.com>
2021-11-19 03:52:36 +0600

    basic

---
+++ Basics/Find next higher natural number that is divisble by y.js
@@ -0,0 +1,16 @@
+// Write a function that takes two numbers, say x and y, as arguments
+// Check if x is divisible by y
+// If yes, return x
+// If not, return the next higher natural number that is divisible by y
+
+function myFunction(x, y) {
+  if (x % y == 0) return x;
+  else {
+    while (x % y != 0) {
+      x++;
+    }
+  }
+  return x;
+}
+
+//Problem link: https://www.jschallenger.com/javascript-basics/find-next-higher-number/
---
+++ Basics/Find the correct word by incrementing letters in alphabe.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string as argument
+// As it is, the string has no meaning
+// Increment each letter to the next letter in the alphabet
+// Return the correct word
+
+function myFunction(string) {
+  arr = [...string];
+  const arr2 = arr.map((item) => String.fromCharCode(item.charCodeAt() + 1));
+  return arr2.join("");
+}
+
+//Problem link: https://www.jschallenger.com/javascript-basics/incrementing-javascript-letters-alphabet/

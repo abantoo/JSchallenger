5ee72a3b097dbf5a141dad3a2ff26e9918d65a0f
abantoo <abantonaimur@gmail.com>
2021-11-19 02:44:46 +0600

    .

---
+++ Basics/Round a number to 2 decimal places.js
@@ -0,0 +1,9 @@
+// Write a function that takes a number as argument
+// Round the number to the 2nd digit after the comma
+// Return the rounded number
+
+function myFunction(number) {
+  return Math.round(number * 100) / 100;
+}
+
+//Problem link: https://www.jschallenger.com/javascript-basics/round-2-decimal-digits/

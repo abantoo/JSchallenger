4d0e161dc8603e1a226c79d01102d804154e0764
abantoo <abantonaimur@gmail.com>
2021-11-19 23:24:00 +0600

    .

---
+++ Arrays/Check if all array elements are equal.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array as argument
+// It should return true if all elements in the array are equal
+// It should return false otherwise
+
+function myFunction(arr) {
+  return arr.every((elm) => elm === arr[0]);
+}
+
+//Problem Link: https://www.jschallenger.com/javascript-arrays/check-if-array-elements-are-equal

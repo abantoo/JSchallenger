4ad30dad7ac29772723ed1d4fffbbd1a35c5903e
abantoo <abantonaimur@gmail.com>
2021-11-19 18:30:29 +0600

    obj

---
+++ Arrays/Merge two arrays with duplicate values.js
@@ -0,0 +1,10 @@
+// Write a function that takes two arrays as arguments
+// Merge both arrays and remove duplicate values
+// Sort the merge result in ascending order
+// Return the resulting array
+
+function myFunction(arr1, arr2) {
+  return [...new Set([...arr1, ...arr2])].sort((a, b) => a - b);
+}
+
+//problem link: https://www.jschallenger.com/javascript-arrays/merge-arrays-dublicates/
---
+++ Arrays/Sum up all array elements with values greater than.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array and a number, say num, as arguments
+// Sum up all array elements with a value greater than num
+// Return the sum
+
+function myFunction(arr, n) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] > n ? (sum += arr[i]) : sum;
+  }
+  return sum;
+}
+
+//Problem link: https://www.jschallenger.com/javascript-arrays/sum-up-array-greater/

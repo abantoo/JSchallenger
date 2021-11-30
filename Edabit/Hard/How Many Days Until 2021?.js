afc8d407d85d2d502535208d200aceb30054691d
abantoo <32523633+abantoo@users.noreply.github.com>
2021-11-30 16:10:07 +0600

    added comment about the conversion from ms to days

--- Edabit/Hard/How Many Days Until 2021?.js
+++ Edabit/Hard/How Many Days Until 2021?.js
@@ -6,8 +6,9 @@ function daysUntil2021(date) {
   const date1 = new Date(date);
   const date2 = new Date("January 1, 2021");
 
-  const diff = (date2 - date1) / (3600000 * 24);
+  // as date returns milliseconds, to convert it into days we need to divide the difference by (1000 * 60 * 60 * 24)
+  const diff = (date2 - date1) / (3600000 * 24); 
 
-  const res = diff === 1 ? `${diff} day` : `${diff} days`;
-  return res;
+  const result = diff === 1 ? `${diff} day` : `${diff} days`;
+  return result;
 }

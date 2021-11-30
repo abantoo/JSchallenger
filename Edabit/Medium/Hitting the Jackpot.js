2516f09bc1938fb87efca6efeb10dc4e9fb98d6a
abantoo <32523633+abantoo@users.noreply.github.com>
2021-11-30 16:33:43 +0600

    Update Edabit/Medium/Hitting the Jackpot.js
    Co-authored-by: Ankur Mursalin <48784228+Ankur2019@users.noreply.github.com>

--- Edabit/Medium/Hitting the Jackpot.js
+++ Edabit/Medium/Hitting the Jackpot.js
@@ -1,4 +1,4 @@
-//https://edabit.com/challenge/hxyvTffvdT4E238CY
+// Problem link: https://edabit.com/challenge/hxyvTffvdT4E238CY
 function testJackpot(result) {
   return result.every((i) => i === result[0]);
 }

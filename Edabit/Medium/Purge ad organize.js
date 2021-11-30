818c0d09617724246ab8420861afdb4bd2f7ecfd
abantoo <32523633+abantoo@users.noreply.github.com>
2021-11-30 16:23:53 +0600

    Update Edabit/Medium/Purge ad organize.js
    added space after comment
    
    Co-authored-by: Ankur Mursalin <48784228+Ankur2019@users.noreply.github.com>

--- Edabit/Medium/Purge ad organize.js
+++ Edabit/Medium/Purge ad organize.js
@@ -1,4 +1,4 @@
-//Problem link: https://edabit.com/challenge/CvPCBc9FtTLix9zov
+// Problem link: https://edabit.com/challenge/CvPCBc9FtTLix9zov
 
 function uniqueSort(arr) {
   return [...new Set(arr.sort((a, b) => a - b))];

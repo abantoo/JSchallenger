ad16051abbad15f0a0888f0942d2767c90dd398b
abantoo <32523633+abantoo@users.noreply.github.com>
2021-11-30 16:21:19 +0600

    Update Edabit/Medium/Remove the Letters ABC.js
    added space
    
    Co-authored-by: Ankur Mursalin <48784228+Ankur2019@users.noreply.github.com>

--- Edabit/Medium/Remove the Letters ABC.js
+++ Edabit/Medium/Remove the Letters ABC.js
@@ -1,4 +1,4 @@
-//Problem link: https://edabit.com/challenge/RftQjZs3srJnRBEH8
+// Problem link: https://edabit.com/challenge/RftQjZs3srJnRBEH8
 
 function removeABC(str) {
   if (str.match(/[abc]/g)) return str.replace(/[abc]/gi, "");

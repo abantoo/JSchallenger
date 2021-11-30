5976363431a0c478d60df1a2046fa2371984cf82
abantoo <32523633+abantoo@users.noreply.github.com>
2021-11-30 18:29:25 +0600

    removed comment

--- Edabit/Medium/Fix the Error: Flattening an Array.js
+++ Edabit/Medium/Fix the Error: Flattening an Array.js
@@ -1,5 +1,5 @@
 // Problem link: https://edabit.com/challenge/MXChCnzCSBobgY4Ex
-// Fix this incorrect code so that all tests pass!
+
 "use strict";
 function flatten(arr) {
   return [...arr[0], ...arr[1]];
